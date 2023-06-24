596f37127f6dafc57641518c326288bd905bc1e4
Robin Löbbert <robin.loebbert@campus.lmu.de>
2023-06-24 12:54:18 +0200

    flair roberta training

--- models/flair_roberta.py
+++ models/flair_roberta.py
@@ -1,3 +1,4 @@
+import pickle
 import sys
 import os
 
@@ -12,6 +13,10 @@ from flair.models import SequenceTagger
 from flair.trainers import ModelTrainer
 
 from data_preparation.lowner import wnut_types
+import lightning.pytorch as pl
+from torch.optim.adamw import AdamW
+from torch.optim.lr_scheduler import OneCycleLR
+import torch
 
 
 def get_lowner_dataset():
@@ -31,7 +36,7 @@ def train_model(data_path: str, corpus: Corpus, seed: int):
     label_dict = corpus.make_label_dictionary(label_type=label_type,
                                               add_unk=False)
     embeddings = TransformerWordEmbeddings(
-        model='roberta-large',
+        model='xlm-roberta-large',
         layers="-1",
         subtoken_pooling="first",
         fine_tune=True,
@@ -50,26 +55,57 @@ def train_model(data_path: str, corpus: Corpus, seed: int):
     trainer = ModelTrainer(tagger, corpus)
 
     model_output_path = os.path.join(data_path, f"seed_{seed}",
-                                     "03_checkpoints")
+                                     "03_checkpoints", "flair_roberta")
     os.makedirs(model_output_path, exist_ok=True)
+    tensorboard_logdir = os.path.sep + os.path.join(
+        os.path.join(*data_path.split(os.path.sep)[:-1]), "lightning_logs",
+        f"{seed}_flair_roberta")
+    os.makedirs(tensorboard_logdir, exist_ok=True)
+    # seed
+    if "PL_GLOBAL_SEED" in os.environ:
+        del os.environ["PL_GLOBAL_SEED"]
+    pl.seed_everything(seed)
     training_result = trainer.fine_tune(
         model_output_path,
-        seed=seed,
-        device="cuda:0",
+        #device="cuda:0",
         use_amp=True,
-        learning_rate=5.0e-6,
-        mini_batch_size=8,
-        eval_batch_size=24,
-        max_epochs=10,
-        mini_batch_chunk_size=1,
-        param_selection_mode=True,
-        monitor_train=True,
+        learning_rate=5e-6,
+        mini_batch_size=40,
+        eval_batch_size=120,
+        max_epochs=20,  # 20
+        mini_batch_chunk_size=15,
+        num_workers=3,
+        optimizer=AdamW,
+        scheduler=OneCycleLR,  # type: ignore
+        embeddings_storage_mode='none',
+        weight_decay=0.,
+        shuffle_first_epoch=True,
+        param_selection_mode=False,
+        use_final_model_for_eval=False,
+        monitor_train=False,
+        monitor_test=False,
         save_final_model=True,
         main_evaluation_metric=("macro avg", "f1-score"),
         use_tensorboard=True,
-        tensorboard_log_dir=os.path.join(data_path, "lightning_logs"),
+        tensorboard_log_dir=tensorboard_logdir,
+        tensorboard_comment=f"{seed}_flair_roberta",
         metrics_for_tensorboard=[("macro avg", "f1-score"),
                                  ("macro avg", "precision"),
                                  ("macro avg", "recall")])
 
     return training_result
+
+
+if __name__ == "__main__":
+    seeds = [2, 3]
+    dataset = get_lowner_dataset()
+    datapath = os.path.join("/home/loebbert/projects/thesis", "experiments",
+                            "01_performance", "data")
+    for seed in seeds:
+        trained = False
+        while not trained:
+            try:
+                train_model(datapath, dataset, seed)
+                trained = True
+            except RuntimeError:
+                torch.cuda.empty_cache()
