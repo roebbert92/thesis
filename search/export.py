dbf094962860ac04be5ad7edf788fbababe7cb17
Robin Löbbert <robin.loebbert@campus.lmu.de>
2023-06-29 15:01:37 +0200

    dict match results

--- search/export.py
+++ search/export.py
@@ -8,7 +8,7 @@ sys.path.append(thesis_path)
 from search.utils import EMBEDDING_DIM
 
 from haystack.document_stores import ElasticsearchDocumentStore
-import pickle
+import json
 
 gaz_doc_store = ElasticsearchDocumentStore(index="gaz",
                                            embedding_dim=EMBEDDING_DIM,
@@ -27,8 +27,15 @@ doc_stores = {
 }
 
 for folder_name, doc_store in doc_stores.items():
-    export_path = os.path.join(thesis_path, "search", folder_name,
-                               "database_export.pkl")
+    if folder_name in ["gaz", "sent"]:
+        export_path = os.path.join(thesis_path, "data", "multiconer",
+                                   f"multiconer_{folder_name}.json")
+    else:
+        export_path = os.path.join(thesis_path, "data", "mlowner",
+                                   "lowner_gazetteer_db.json")
     os.makedirs(os.path.dirname(export_path), exist_ok=True)
-    with open(export_path, "wb") as file:
-        pickle.dump(doc_store.get_all_documents(), file)
+    with open(export_path, "w", encoding="utf-8") as file:
+        json.dump([{
+            "content": str(doc.content),
+            "meta": doc.meta
+        } for doc in doc_store.get_all_documents()], file)
