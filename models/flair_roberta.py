627a8ceca2affc2676870e49288501737d401ac6
Robin Löbbert <robin.loebbert@campus.lmu.de>
2023-07-07 16:58:34 +0200

    experiment 03 flair results

--- models/flair_roberta.py
+++ models/flair_roberta.py
@@ -1,8 +1,10 @@
 import gc
 from glob import glob
 import pickle
+import shutil
 import sys
 import os
+import time
 from typing import List, Optional
 
 thesis_path = "/" + os.path.join(
@@ -36,7 +38,7 @@ def get_lowner_dataset():
         dev_file="lowner_dev.bio",
         test_file="lowner_test.bio",
         #label_name_map=wnut_types
-    )
+        in_memory=False)
     return corpus
 
 
@@ -50,7 +52,7 @@ def get_wnut_dataset():
         dev_file="wnut_dev.bio",
         test_file="wnut_test.bio",
         #label_name_map=wnut_types
-    )
+        in_memory=False)
     return corpus
 
 
@@ -61,89 +63,101 @@ def train_model(corpus: Corpus,
                 model_output_path: str,
                 metrics_base_path: str,
                 ckpt_path: Optional[str] = None,
-                num_epochs: int = 20):
-    if "PL_GLOBAL_SEED" in os.environ:
-        del os.environ["PL_GLOBAL_SEED"]
-    pl.seed_everything(seed)
-    if ckpt_path is None:
-        label_type = "ner"
-        label_dict = corpus.make_label_dictionary(label_type=label_type,
-                                                  add_unk=False)
-        embeddings = TransformerWordEmbeddings(
-            model='xlm-roberta-large',
-            layers="-1",
-            subtoken_pooling="first",
-            fine_tune=True,
-            use_context=True,
-        )
-        tagger = SequenceTagger(
-            hidden_size=256,
-            embeddings=embeddings,
-            tag_dictionary=label_dict,
-            tag_type='ner',
-            tag_format="BIO",
-            use_crf=False,
-            use_rnn=False,
-            reproject_embeddings=False,
-        )
-    else:
-        tagger = SequenceTagger.load(ckpt_path)
-    # seed
-    mini_batch_chunk_size = 6
-    try:
-        trainer = ModelTrainer(tagger, corpus)
-        trainer.fine_tune(
-            model_output_path,
-            #device="cuda:0",
-            use_amp=True,
-            learning_rate=5e-6,
-            mini_batch_size=40,
-            eval_batch_size=40,
-            max_epochs=num_epochs,  # 20
-            mini_batch_chunk_size=mini_batch_chunk_size,
-            num_workers=3,
-            optimizer=AdamW,
-            scheduler=OneCycleLR,  # type: ignore
-            embeddings_storage_mode='none',
-            weight_decay=0.,
-            shuffle_first_epoch=True,
-            param_selection_mode=False,
-            use_final_model_for_eval=False,
-            monitor_train=False,
-            monitor_test=False,
-            save_final_model=True,
-            exclude_labels=["O"],
-            main_evaluation_metric=("macro avg", "f1-score"),
-            use_tensorboard=True,
-            tensorboard_log_dir=tensorboard_logdir,
-            tensorboard_comment=f"{seed}_flair_roberta",
-            metrics_for_tensorboard=[("macro avg", "f1-score"),
-                                     ("macro avg", "precision"),
-                                     ("macro avg", "recall")])
-    except RuntimeError:
-        mini_batch_chunk_size = mini_batch_chunk_size - 1 if mini_batch_chunk_size > 1 else 1
-        try:
-            del trainer
-        except:
-            pass
-        gc.collect()
-        torch.cuda.empty_cache()
+                num_epochs: int = 20,
+                finetuning: str = "full"):
+    last_ckpt_path = os.path.join(model_output_path, "final-model.pt")
+    best_ckpt_path = os.path.join(model_output_path, "best-model.pt")
+    if not os.path.exists(last_ckpt_path) and not os.path.exists(
+            best_ckpt_path):
+        if "PL_GLOBAL_SEED" in os.environ:
+            del os.environ["PL_GLOBAL_SEED"]
+        pl.seed_everything(seed)
+        if finetuning == "full":
+            if ckpt_path is None:
+                label_type = "ner"
+                label_dict = corpus.make_label_dictionary(
+                    label_type=label_type, add_unk=False)
+                embeddings = TransformerWordEmbeddings(
+                    model='xlm-roberta-large',
+                    layers="-1",
+                    subtoken_pooling="first",
+                    fine_tune=True,
+                    use_context=True,
+                )
+                tagger = SequenceTagger(
+                    hidden_size=256,
+                    embeddings=embeddings,
+                    tag_dictionary=label_dict,
+                    tag_type='ner',
+                    tag_format="BIO",
+                    use_crf=False,
+                    use_rnn=False,
+                    reproject_embeddings=False,
+                )
+            else:
+                tagger = SequenceTagger.load(ckpt_path)
+            # seed
+            mini_batch_chunk_size = 6
+            try:
+                trainer = ModelTrainer(tagger, corpus)
+                trainer.fine_tune(
+                    model_output_path,
+                    #device="cuda:0",
+                    use_amp=True,
+                    learning_rate=5e-6,
+                    mini_batch_size=40,
+                    eval_batch_size=40,
+                    max_epochs=num_epochs,  # 20
+                    mini_batch_chunk_size=mini_batch_chunk_size,
+                    num_workers=3,
+                    optimizer=AdamW,
+                    scheduler=OneCycleLR,  # type: ignore
+                    embeddings_storage_mode='none',
+                    weight_decay=0.,
+                    shuffle_first_epoch=True,
+                    param_selection_mode=False,
+                    use_final_model_for_eval=False,
+                    monitor_train=False,
+                    monitor_test=False,
+                    save_final_model=True,
+                    exclude_labels=["O"],
+                    main_evaluation_metric=("macro avg", "f1-score"),
+                    use_tensorboard=True,
+                    tensorboard_log_dir=tensorboard_logdir,
+                    tensorboard_comment=f"{seed}_flair_roberta",
+                    metrics_for_tensorboard=[("macro avg", "f1-score"),
+                                             ("macro avg", "precision"),
+                                             ("macro avg", "recall")])
+            except RuntimeError:
+                mini_batch_chunk_size = mini_batch_chunk_size - 1 if mini_batch_chunk_size > 1 else 1
+                try:
+                    del trainer
+                except:
+                    pass
+                gc.collect()
+                torch.cuda.empty_cache()
 
-    try:
-        del trainer
-    except:
-        pass
-    gc.collect()
-    torch.cuda.empty_cache()
+            try:
+                del tagger
+                del trainer
+            except:
+                pass
+            gc.collect()
+            torch.cuda.empty_cache()
+        elif finetuning == "no" and ckpt_path is not None:
+            ckpt_dir_path = os.path.dirname(ckpt_path)
+            for file_name in ["best-model.pt", "final-model.pt"]:
+                shutil.copy(os.path.join(ckpt_dir_path, file_name),
+                            os.path.join(model_output_path, file_name))
 
     # metrics
     # get metrics for last ckpt
-    last_ckpt_path = os.path.join(model_output_path, "final-model.pt")
+    while not os.path.exists(last_ckpt_path):
+        time.sleep(5)
     get_asp_metrics_from_flair(metrics_base_path, evaluation_dataset_names,
                                corpus, "last", last_ckpt_path)
 
     # get metrics for best ckpt
-    best_ckpt_path = os.path.join(model_output_path, "best-model.pt")
     get_asp_metrics_from_flair(metrics_base_path, evaluation_dataset_names,
                                corpus, "best", best_ckpt_path)
 
@@ -151,8 +165,6 @@ def train_model(corpus: Corpus,
 def get_asp_metrics_from_flair(metrics_base_path: str, dataset_names: list,
                                flair_dataset: Corpus, ckpt_name: str,
                                ckpt_path: str):
-
-    tagger = SequenceTagger.load(ckpt_path)
 
     os.makedirs(metrics_base_path, exist_ok=True)
 
@@ -166,16 +178,22 @@ def get_asp_metrics_from_flair(metrics_base_path: str, dataset_names: list,
         elif dataset_part == "test":
             flair_data = flair_dataset.test
         mini_batch_size = 40
-        try:
-            tagger.evaluate(
-                flair_data,  # type: ignore
-                "ner",
-                out_path=temp_path,
-                return_loss=False,
-                mini_batch_size=mini_batch_size)
-        except RuntimeError:
-            mini_batch_size = mini_batch_size - 1 if mini_batch_size > 1 else 1
-            torch.cuda.empty_cache()
+        evaluated = False
+        while not evaluated:
+            try:
+                tagger = SequenceTagger.load(ckpt_path)
+                tagger.evaluate(
+                    flair_data,  # type: ignore
+                    "ner",
+                    out_path=temp_path,
+                    return_loss=False,
+                    mini_batch_size=mini_batch_size)
+                evaluated = True
+            except RuntimeError:
+                del tagger
+                gc.collect()
+                mini_batch_size = mini_batch_size - 1 if mini_batch_size > 1 else 1
+                torch.cuda.empty_cache()
         # init metrics
         metrics = ASPMetrics()
         metrics.predictions = asp_dataset_to_asp_predictions(
@@ -205,9 +223,6 @@ def experiment_01():
                                                  "03_checkpoints",
                                                  "flair_roberta")
                 os.makedirs(model_output_path, exist_ok=True)
-                ckpt_files = list(glob(os.path.join(model_output_path, "*")))
-                for ckpt_file in ckpt_files:
-                    os.remove(ckpt_file)
                 metrics_base_path = os.path.join(datapath, f"seed_{seed}",
                                                  "04_metrics", "flair_roberta")
                 os.makedirs(metrics_base_path, exist_ok=True)
@@ -215,6 +230,14 @@ def experiment_01():
                     os.path.join(*datapath.split(os.path.sep)[:-1]),
                     "lightning_logs", f"{seed}_flair_roberta")
                 os.makedirs(tensorboard_logdir, exist_ok=True)
+                ckpt_files = list(glob(os.path.join(model_output_path, "*")))
+                if os.path.join(
+                        model_output_path,
+                        "final-model.pt") not in ckpt_files and os.path.join(
+                            model_output_path,
+                            "best-model.pt") not in ckpt_files:
+                    for ckpt_file in ckpt_files:
+                        os.remove(ckpt_file)
                 train_model(dataset, seed, dataset_names, tensorboard_logdir,
                             model_output_path, metrics_base_path)
                 trained = True
@@ -251,21 +274,16 @@ def experiment_03():
     ]
     for seed in seeds:
         for gaz, finetuning, pretrained, config in configs:
-            if (1, False, "full", False) == (seed, gaz, finetuning,
-                                             pretrained):
-                continue
             trained = False
             while not trained:
                 try:
+                    torch.cuda.empty_cache()
+                    gc.collect()
                     model_output_path = os.path.join(
                         config["data_path"], f"seed_{str(seed)}",
                         "03_checkpoints",
                         f"{gaz}_{finetuning}_{pretrained}_{config['name']}")
                     os.makedirs(model_output_path, exist_ok=True)
-                    ckpt_files = list(
-                        glob(os.path.join(model_output_path, "*")))
-                    for ckpt_file in ckpt_files:
-                        os.remove(ckpt_file)
                     metrics_base_path = os.path.join(
                         config["data_path"], f"seed_{str(seed)}", "04_metrics",
                         f"{gaz}_{finetuning}_{pretrained}_{config['name']}",
@@ -277,11 +295,19 @@ def experiment_03():
                         f"{seed}_{gaz}_{finetuning}_{pretrained}_{config['name']}_-1"
                     )
                     os.makedirs(tensorboard_logdir, exist_ok=True)
+                    ckpt_files = list(
+                        glob(os.path.join(model_output_path, "*")))
+                    if os.path.join(model_output_path, "final-model.pt"
+                                    ) not in ckpt_files and os.path.join(
+                                        model_output_path,
+                                        "best-model.pt") not in ckpt_files:
+                        for ckpt_file in ckpt_files:
+                            os.remove(ckpt_file)
                     train_model(dataset, seed, dataset_names,
                                 tensorboard_logdir, model_output_path,
                                 metrics_base_path,
                                 config["ckpt_path"] if pretrained else None,
-                                40)
+                                40, finetuning)
                     trained = True
                 except RuntimeError:
                     torch.cuda.empty_cache()
